88f60b13376435be8bc779bf37246f910263db61
Jeko Paray <jdparay@mcm.edu.ph>
2026-06-18 21:33:40 +0800

    title screen animated

---
+++ Assets/LogoScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoScript : MonoBehaviour
+{
+    private Animator animator;
+    private static bool hasPlayedStartAnimation = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+        if (!hasPlayedStartAnimation)
+        {
+            animator.SetTrigger("StartAnimation");
+            hasPlayedStartAnimation = true;
+            StartCoroutine(PlayStartThenIdle());
+        }
+        else
+        {
+            animator.SetTrigger("IdleAnimation");
+        }
+    }
+
+    private IEnumerator PlayStartThenIdle()
+    {
+        // Wait for the start animation to complete
+        yield return new WaitForSeconds(GetAnimationLength("LogoStart"));
+        
+        // Transition to idle animation
+        animator.SetTrigger("IdleAnimation");
+    }
+
+    private float GetAnimationLength(string animationName)
+    {
+        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        {
+            if (clip.name == animationName)
+                return clip.length;
+        }
+        return 1f; // Default fallback
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
